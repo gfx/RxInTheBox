c188f4f399c04cdee9ac51919b0069f5e077babf
FUJI Goro (gfx) <gfuji@cpan.org>
2015-03-17 00:30:19 +0900

    initial commit

---
+++ app/src/main/java/com/github/gfx/rxinthebox/MainActivity.java
@@ -0,0 +1,81 @@
+package com.github.gfx.rxinthebox;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+class Observable<T> {
+    static <U> Observable<U> create(OnSubscribe<U> onSubscribe) {
+        return new Observable<>(onSubscribe);
+    }
+
+    OnSubscribe<T> onSubscribe;
+
+    Observable(OnSubscribe<T> onSubscribe) {
+        this.onSubscribe = onSubscribe;
+    }
+
+    interface OnSubscribe<T> {
+        void call(Subscriber<T> subscriber);
+    }
+
+    void subscribe(Subscriber<T> subscriber) {
+        onSubscribe.call(subscriber);
+    }
+}
+
+interface Subscriber<T>  {
+    void onNext(T value);
+    void onComplete();
+    void onError(Throwable e);
+}
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        doit();
+    }
+
+    Observable<String> getSomethingDelayed(final String value) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(final Subscriber<String> subscriber) {
+                // do something important here
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        subscriber.onNext(value);
+                    }
+                }, 1000);
+            }
+        });
+    }
+
+    void doit() {
+        Observable<String> observable = getSomethingDelayed("foo");
+
+        observable.subscribe(new Subscriber<String>() {
+            @Override
+            public void onNext(String value) {
+                Toast.makeText(MainActivity.this, "onNext: " + value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this, "onComplete", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
